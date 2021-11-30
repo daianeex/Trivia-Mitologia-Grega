b0899ac50fa2eef94fc6d9f5f39581b4431dda89
daianeex <95250588+daianeex@users.noreply.github.com>
2021-11-30 15:26:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,75 +1,19 @@
-let titulo = document.querySelector ('h1')
-let instrucoes = document.querySelector ('#instrucoes')
+var answers = {};
 
-//PERGUNTA
-let numQuestao = document.querySelector ('#numQuestao')
-let pergunta = document.querySelector ('#pergunta')
+var question_one = document.getElementById('question-1');
+var question_two = document.getElementById('question-2');
 
-//ALTERNATIVAS
-let a = document.querySelector ('#a')
-let b = document.querySelector ('#b')
-let c = document.querySelector ('#c')
-
-//article com a Class questoes
-let articleQuestoes = document.querySelector ('.questao')
-// o1 li com as alternativas
-let alternativas = document.querySelector ('#ALTERNATIVAS')
-
-const q1 = {
-  numQuestao : q1,
-  pergunta   : "Quais foram os três grandes deuses que derrotaram Cronos?",
-  alternativaA : "Apolo, Hermes e Hefesto",
-  alternativaB : "Zeus, Hades e Poseidon",
-  alternativaC : "Poseidon, Dioniso e Ares",
-  correta      : "Zeus, Hades e Poseidon",
+function storeAnswer(question_number, event){
+    if(event.target.type === 'radio'){
+        console.log(event.target.value);
+        answers['question'+question_number] = parseInt(event.target.value);
+        console.log(answers);
+    }
 }
-const q2 = {
-  numQuestao : q2,
-  pergunta   : "Quais foram os três grandes deuses que derrotaram Cronos?",
-  alternativaA : "Apolo, Hermes e Hefesto",
-  alternativaB : "Zeus, Hades e Poseidon",
-  alternativaC : "Poseidon, Dioniso e Ares",
-  correta      : "Zeus, Hades e Poseidon",
-}
-//CONSTANTE
-const questoes = [q1, q2]
 
-let numero = document.querySelector ('#numero')
-let total = document.querySelector ('#total')
-
-numero.textContent = q1.numQuestao
-
-let totalDeQuestaoes = (questoes.length)-1
-console.log("Total de questões" + totalDeQuestoes)
-total.textContent = totalDeQuestoes
-
-// 1a Questao COmpleta
-numQuestao.textContent = q1.numQuestao
-pergunta.textContent = q1.pergunta
-a.textContent = q1.alternativaA
-b.textContent = q1.alternativaB
-c.textContent = q1.alternativaC
-
-//CONFIGURAR O VALUE INICIAL
-a.setAttribute ('value', '1A')
-b.setAttribute ('value', '1B')
-c.setAttribute ('value', '1C')
-
-function verifcarSeAcertou (nQuestao, resposta) {
-  let numeroDaQuestao = nQuestao.value
-  console.log ("Questão " + numeroDaQuestao)
-
-  let respostaEscolhida = resposta.textContent
-  console.log ("Resp " + respostaEscolhida)
-
-  let certa = questoes[numeroDaQuestao].correta
-  console.log("RespC" + certa)
-
-  if(respostaEscolhida == certa) {
-    //console.log ("Acertou")
-    //respostaEsta.textContent = "Correta"
-
-  } else {
-    //console.log("Errou")
-    //respostaEsta.textContent = "Errada"
-  }
+question_one.addEventListener('click', function(event){
+    storeAnswer(1, event)
+})
+question_two.addEventListener('click', function(event){
+    storeAnswer(2, event)
+})
