644063ea1f56e1bf286d85e90139bb90c60fe1fa
daianeex <95250588+daianeex@users.noreply.github.com>
2021-11-29 22:11:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ var answers = ();
 
 var question_one = document.getElementById ('question-1');
 var question_two = document.getElementById ('question-2');
+
+function storeAnswers (question_number, event){
+  
+}
