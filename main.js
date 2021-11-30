1dc90b142c63a6b4a642e40a7b3784087c4c4a19
daianeex <95250588+daianeex@users.noreply.github.com>
2021-11-30 12:49:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let titulo = document.querySelector ('h1')
-let instrucoes = document.querySelector ('#instrucoes')                                     
-let aviso = document.querySelector ('aviso')
+let instrucoes = document.querySelector ('#instrucoes')
 
 //PERGUNTA
-let Questao1 = document.querySelector ('#Questao1')
+let numQuestaouestion = document.querySelector ('#numQuestao')
 let pergunta = document.querySelector ('#pergunta')
 
 //ALTERNATIVAS
@@ -12,27 +11,27 @@ let b = document.querySelector ('#b')
 let c = document.querySelector ('#c')
 
 //article com a Class questoes
-let articleQuestoes = document.querySelector ('questoes')
+let articleQuestoes = document.querySelector ('.questao')
 // o1 li com as alternativas
-let alternativas = document.querySelector ('#alternativas')
+let alternativas = document.querySelector ('#ALTERNATIVAS')
 
-cons q1 = {
-  numQuestao : 1
-  pergunta   : "Quais foram os três grandes deuses que derrotaram Cronos?"
-  alternativaA : "Apolo, Hermes e Hefesto"
-  alternativaB : "Zeus, Hades e Poseidon"
-  alternativaC : "Poseidon, Dioniso e Ares"
-  correta      : "Zeus, Hades e Poseidon"
+const q1 = {
+  numQuestao : q1,
+  pergunta   : "Quais foram os três grandes deuses que derrotaram Cronos?",
+  alternativaA : "Apolo, Hermes e Hefesto",
+  alternativaB : "Zeus, Hades e Poseidon",
+  alternativaC : "Poseidon, Dioniso e Ares",
+  correta      : "Zeus, Hades e Poseidon",
 }
-cons q2 = {
-  numQuestao : 2
-  pergunta   : "Quais foram os três grandes deuses que derrotaram Cronos?"
-  alternativaA : "Apolo, Hermes e Hefesto"
-  alternativaB : "Zeus, Hades e Poseidon"
-  alternativaC : "Poseidon, Dioniso e Ares"
-  correta      : "Zeus, Hades e Poseidon"
+const q2 = {
+  numQuestao : q2,
+  pergunta   : "Quais foram os três grandes deuses que derrotaram Cronos?",
+  alternativaA : "Apolo, Hermes e Hefesto",
+  alternativaB : "Zeus, Hades e Poseidon",
+  alternativaC : "Poseidon, Dioniso e Ares",
+  correta      : "Zeus, Hades e Poseidon",
 }
-//CONSTANTE 
+//CONSTANTE
 const questoes = [q1, q2]
 
 let numero = document.querySelector ('#numero')
@@ -51,7 +50,7 @@ a.textContent = q1.alternativaA
 b.textContent = q1.alternativaB
 c.textContent = q1.alternativaC
 
-//CONFIGURAR O VALUE INICIAL 
+//CONFIGURAR O VALUE INICIAL
 a.setAttribute ('value', '1A')
 b.setAttribute ('value', '1B')
 c.setAttribute ('value', '1C')
@@ -59,19 +58,18 @@ c.setAttribute ('value', '1C')
 function verifcarSeAcertou (nQuestao, resposta) {
   let numeroDaQuestao = nQuestao.value
   console.log ("Questão " + numeroDaQuestao)
-  
+
   let respostaEscolhida = resposta.textContent
   console.log ("Resp " + respostaEscolhida)
-  
+
   let certa = questoes[numeroDaQuestao].correta
   console.log("RespC" + certa)
-  
+
   if(respostaEscolhida == certa) {
     //console.log ("Acertou")
     //respostaEsta.textContent = "Correta"
-    
+
   } else {
     //console.log("Errou")
     //respostaEsta.textContent = "Errada"
   }
-  
