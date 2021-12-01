8a59821a13167fa73692ec8f45901def9806cc1a
Daianex <95250588+daianeex@users.noreply.github.com>
2021-12-01 15:45:49 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,22 @@
+myFunction ()
+{
+//Respostacorreta
+if (Respostacorreta == "question-1-answer-b") {
+    prompt('Você acertou')
+  }
+//Respostaerrada
+if (Respostaerrada == "question-1-answer-a") {:
+  prompt('Você errou')
+}
+
+//Respostacorreta
+if (Respostacorreta == "question-1-answer-c") {
+    prompt('Você errou')
+  }
+
+else {
+  prompt('resposta errada')
+}
+var document.querySelector('question-1-answer-b').value;
+
+var document.querySelector("question-2-answer-a").value;
